1010c4b0033e2d1e33ab9a554b75d94bfe10dcaa
Jeff SH Wang <jeff_sh_wang@wistron.com>
2020-04-22 17:33:46 +0800

    Add project_api

--- develop_requirement_proj/signature/api/serializers.py
+++ develop_requirement_proj/signature/api/serializers.py
@@ -2,10 +2,30 @@
 from rest_framework import serializers
 
 
-class AccountSerializer(serializers.Serializer):
+class AccountBaseSerializer(serializers.Serializer):
     """ Account Serializer """
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(read_only=True)
     code = serializers.CharField(read_only=True)
-    business_unit = serializers.CharField(read_only=True)
+    business_unit = serializers.IntegerField(read_only=True)
+
+
+class AccountSerializer(AccountBaseSerializer):
     project_count = serializers.IntegerField(read_only=True)
+
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    wistron_name = serializers.CharField(read_only=True)
+    customer_name = serializers.CharField(read_only=True)
+    wistron_code = serializers.CharField(read_only=True)
+    plm_code_1 = serializers.CharField(read_only=True)
+    plm_code_2 = serializers.CharField(read_only=True)
+    deleted_at = serializers.CharField(read_only=True)
+    name = serializers.CharField(read_only=True)
+    plm_code = serializers.CharField(read_only=True)
+    type = serializers.CharField(read_only=True)
+    status = serializers.CharField(read_only=True)
+    product_line = serializers.CharField(read_only=True)
+    business_model = serializers.CharField(read_only=True)
+    account = AccountBaseSerializer()
